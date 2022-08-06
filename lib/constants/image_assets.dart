cc0c4153655c840280aee3998ab898ba34c7822c
Kit <kit.jrs@gmail.com>
2022-08-06 17:45:23 +0700

    fixed

--- lib/constants/image_assets.dart
+++ lib/constants/image_assets.dart
@@ -1,13 +1,7 @@
 class ImageAssets {
   static const String imageDir = "assets/images";
 
-  // static const String logo = "$imageDir/logo.png";
-  // static const String logoName = "$imageDir/logo-name.png";
-  // static const String logoLcmt = "$imageDir/lcmt_logo.png";
-  // static const String logoLcb1 = "$imageDir/lcb1_logo.png";
-  // static const String background = "$imageDir/background.png";
   static const String backgroundTop = "$imageDir/background-top.png";
   static const String backgroundBottom = "$imageDir/background-bottom.png";
   static const String headerBackground = "$imageDir/header-background.png";
-  // static const String imageNotFound = "$imageDir/no-image.png";
 }
